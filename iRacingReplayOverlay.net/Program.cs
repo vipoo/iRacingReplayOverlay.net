fae1ccb754ed136bd82461522a5582e829381223
Dean Netherton <antispam@netherton.org>
2014-04-10 21:21:08 +1000

    initial commit

---
+++ iRacingReplayOverlay.net/Program.cs
@@ -0,0 +1,105 @@
+﻿using MediaFoundation;
+using MediaFoundation.Misc;
+using MediaFoundation.ReadWrite;
+using MediaFoundation.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaFoundation.Transform;
+using System.Drawing;
+
+
+namespace iRacingReplayOverlay.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using( MediaFoundation.Net.MFSystem.Start() )
+            {
+                var readWriteFactory = new ReadWriteClassFactory();
+            
+                var attributes = new Attributes(1);
+
+                attributes.ReadWriterEnableHardwareTransforms = false;
+                attributes.SourceReaderEanbleVideoProcessing = true;
+
+                var sourceReader = readWriteFactory.CreateSourceReaderFromURL(@"C:\Users\dean\Documents\iRacingShort.mp4", attributes);
+                var sinkWriter = readWriteFactory.CreateSinkWriterFromURL(@"C:\Users\dean\documents\output.wmv", attributes);
+
+				GetDuration (sourceReader, sinkWriter);
+
+				sinkWriter.Dispose();
+            }
+        }
+
+		static void GetDuration(SourceReader sourceReader, SinkWriter sinkWriter)
+		{
+			var duration = sourceReader.MediaSource.Duration;
+
+			int count = 0;
+			foreach (var s in sourceReader.Streams)
+				if (s.IsSelected)
+					count++;
+
+			foreach (var stream in sourceReader.Streams.Where(s => s.IsSelected))
+			{
+				var nativeMediaType = stream.GetNativeMediaType( 0 );
+
+				var isVideo = nativeMediaType.IsVideo;
+				var isAudio = nativeMediaType.IsAudio;
+
+				MediaType targetType;
+
+				if (isAudio)
+					targetType = CreateTargetAudioMediaType (nativeMediaType);
+				else if (isVideo)
+					targetType = CreateTargetVideoMediaType (nativeMediaType);
+				else
+					continue;
+
+				sinkWriter.AddStream (targetType);
+			}
+		}
+
+		static Guid TARGET_AUDIO_FORMAT = MFMediaType.WMAudioV9;
+		static Guid TARGET_VIDEO_FORMAT = MFMediaType.WMV3;
+
+		public static MediaType CreateTargetAudioMediaType(MediaType nativeMediaType)
+		{
+			var numberOfChannels = nativeMediaType.AudioNumberOfChannels;
+			var sampleRate = nativeMediaType.AudioSamplesPerSecond;
+
+			var availableTypes = MFSystem.TranscodeGetAudioOutputAvailableTypes (TARGET_AUDIO_FORMAT, MFT_EnumFlag.All);
+
+			var type = availableTypes
+				.FirstOrDefault (t => t.AudioNumberOfChannels == numberOfChannels && t.AudioSamplesPerSecond == sampleRate);
+
+			if( type != null )
+				return new MediaType (type);
+
+			throw new Exception ("Unable to find target audio format");
+		}
+
+		public static MediaType CreateTargetVideoMediaType(MediaType nativeMediaType)
+		{
+			var size = nativeMediaType.FrameSize;
+			var rate = nativeMediaType.FrameRate;
+			var aspect = nativeMediaType.AspectRatio;
+			var bitRate = nativeMediaType.BitRate;
+
+			var mediaType = new MediaType () {
+				MajorType = MFMediaType.Video,
+				SubType = TARGET_VIDEO_FORMAT,
+				FrameSize = size,
+				FrameRate = rate,
+				AspectRatio = aspect,
+				BitRate = bitRate
+			};
+		
+			return mediaType;
+		}
+    }
+}
