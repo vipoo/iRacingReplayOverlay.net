594144694c03b52c0baff4dc0224f4db82b6be6e
Dean Netherton <dean.netherton@gmail.com>
2014-10-04 18:23:10 +1000

    Published version 1.0.0.14

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
-/c/Program\ Files\ \(x86\)/MSBuild/12.0/Bin/MSBuild.exe iRacingReplayOverlay.net.csproj -t:rebuild -t:publish -p:MSBuildExtensionsPath=/c/Program\ Files/MSBuild/ -p:ApplicationVersion=1.0.0.12
+/c/Program\ Files\ \(x86\)/MSBuild/12.0/Bin/MSBuild.exe iRacingReplayOverlay.net.csproj -t:rebuild -t:publish -p:MSBuildExtensionsPath=/c/Program\ Files/MSBuild/ -p:ApplicationVersion=1.0.0.14
 rm -rf publish/Application\ Files/
 mv bin/x64/Debug/app.publish/* publish
