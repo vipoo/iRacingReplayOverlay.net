63b0a5619273d2cb0acd2d3031bb4bcf72a79eba
Dean Netherton <dean.netherton@gmail.com>
2014-12-13 15:50:32 +1100

    Published version 22

--- publish-beta.sh
+++ publish-beta.sh
@@ -1,3 +1,3 @@
-/c/Program\ Files\ \(x86\)/MSBuild/12.0/Bin/MSBuild.exe iRacingReplayOverlay.net.csproj -t:rebuild -t:publish -p:MSBuildExtensionsPath=/c/Program\ Files/MSBuild/ -p:ApplicationVersion=1.0.0.21	 -p:"InstallUrl=http://iracingreplaydirector-beta.s3-ap-southeast-2.amazonaws.com/" -p:ProductName="iRacing Replay Director (beta)" -p:OverrideAssemblyName=iRacingReplayOverlay.beta
+/c/Program\ Files\ \(x86\)/MSBuild/12.0/Bin/MSBuild.exe iRacingReplayOverlay.net.csproj -t:rebuild -t:publish -p:MSBuildExtensionsPath=/c/Program\ Files/MSBuild/ -p:ApplicationVersion=1.0.0.22 -p:"InstallUrl=http://iracingreplaydirector-beta.s3-ap-southeast-2.amazonaws.com/" -p:ProductName="iRacing Replay Director (beta)" -p:OverrideAssemblyName=iRacingReplayOverlay.beta
 rm -rf publish-beta/Application\ Files/
 mv bin/x64/Debug/app.publish/* publish-beta
