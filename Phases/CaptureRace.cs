0ee05e746f6ebf6116303e452ffd64e35d31351f
Dean Netherton <dean.netherton@gmail.com>
2015-07-05 14:33:53 +1000

    Fixed bug where camera locks onto cars sitting in pits at end of race

--- Phases/CaptureRace.cs
+++ Phases/CaptureRace.cs
@@ -76,7 +76,7 @@ namespace iRacingReplayOverlay.Phases
                 .WithFastestLaps()
                 .WithFinishingStatus()
                 .WithPitStopCounts()
-                .TakeUntil(3.Seconds()).After(d => d.Telemetry.RaceCars.All(c => c.HasSeenCheckeredFlag || c.HasRetired))
+                .TakeUntil(3.Seconds()).After(d => d.Telemetry.RaceCars.All(c => c.HasSeenCheckeredFlag || c.HasRetired || c.TrackSurface == TrackLocation.InPitStall))
                 .TakeUntil(3.Seconds()).AfterReplayPaused();
 
             bool haveSkipForTesting = false;
