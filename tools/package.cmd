fa4ee1a19a17a0f8cae0a2427861aba08fb7e7ab
Dean Netherton <dean.netherton@gmail.com>
2015-12-19 10:15:18 +1100

    Renamed publish script to package, to create artifact on appveyor

---
+++ tools/package.cmd
@@ -0,0 +1,5 @@
+
+msbuild iRacingReplayOverlay.net.csproj -t:publish -p:ApplicationVersion=%APPVEYOR_BUILD_NUMBER% -v:minimal
+rd /S /Q "deploy-package\Application Files"
+move "bin\x64\Debug\app.publish\*" deploy-package
+move "bin\x64\Debug\app.publish\Application Files" "deploy-package\Application Files"
--- tools/publish.cmd
+++
@@ -1,5 +0,0 @@
-
-msbuild iRacingReplayOverlay.net.csproj  -t:rebuild -t:publish -p:ApplicationVersion=1.0.0.10 -v:minimal
-rd /S /Q "publish\Application Files"
-move "bin\x64\Debug\app.publish\*" publish
-move "bin\x64\Debug\app.publish\Application Files" "publish\Application Files"
