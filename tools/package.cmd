015248454eea23a06e47e165ae2cbd974d3a87da
Dean Netherton <dean.netherton@gmail.com>
2015-12-19 10:27:19 +1100

    Fixed package script to use correct appveyor env var

--- tools/package.cmd
+++ tools/package.cmd
@@ -1,6 +1,6 @@
 
 echo 'Building package' %APPVEYOR_BUILD_NUMBER%
-msbuild iRacingReplayOverlay.net.csproj -t:publish -p:ApplicationVersion=%APPVEYOR_BUILD_NUMBER% -v:minimal
+msbuild iRacingReplayOverlay.net.csproj -t:publish -p:ApplicationVersion=%APPVEYOR_BUILD_VERSION% -v:minimal
 rd /S /Q "deploy-package\Application Files"
 move "bin\x64\Debug\app.publish\*" deploy-package
 move "bin\x64\Debug\app.publish\Application Files" "deploy-package\Application Files"
