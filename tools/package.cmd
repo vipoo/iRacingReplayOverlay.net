820b0d4a3e351328841b0f536d141eba83468651
Dean Netherton <dean.netherton@gmail.com>
2015-12-19 10:22:13 +1100

    Echo the build number during packaging

--- tools/package.cmd
+++ tools/package.cmd
@@ -1,4 +1,5 @@
 
+echo 'Building package' %APPVEYOR_BUILD_NUMBER%
 msbuild iRacingReplayOverlay.net.csproj -t:publish -p:ApplicationVersion=%APPVEYOR_BUILD_NUMBER% -v:minimal
 rd /S /Q "deploy-package\Application Files"
 move "bin\x64\Debug\app.publish\*" deploy-package
