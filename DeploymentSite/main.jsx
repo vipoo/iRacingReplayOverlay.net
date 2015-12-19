b0257fd0e1f984107b1ea8a7139a962f43d6ab27
Dean Netherton <dean.netherton@gmail.com>
2015-12-19 22:25:39 +1100

    New web page to list all versions of main exe

---
+++ DeploymentSite/main.jsx
@@ -0,0 +1,40 @@
+﻿import React from 'react'
+import ReactDOM from 'react-dom'
+import 'whatwg-fetch'
+import xmlToJson from './xmlToJson'
+
+const request = new Request('https://s3-ap-southeast-2.amazonaws.com/iracingreplaydirector-test?delimiter=/&prefix=versions/', {
+  headers: new Headers({
+    'Accept': 'application/json'
+  })
+})
+
+const Listing = React.createClass({
+  
+  render() {
+    const contents = this.props.listings.ListBucketResult[0].Contents
+    const keys = contents.map( c => c.Key[0]._text)
+    const rows = keys.map(key => <tr key={key}><td>{key}</td></tr>)
+
+    return (
+     <table>
+     <thead>
+     <tr><th>
+     Files
+     </th></tr>
+     </thead>
+     <tbody>
+     {rows}
+     </tbody>
+     </table>
+    )
+  }
+
+})
+
+
+fetch(request)
+  .then(resp => resp.text())
+  .then(text => xmlToJson.parseString(text))
+  .then(json => ReactDOM.render(<Listing listings={json} />, document.getElementById('listing')))
+
--- tools/package.cmd
+++ tools/package.cmd
@@ -3,18 +3,17 @@ echo 'Building test package' %APPVEYOR_BUILD_NUMBER%
 
 msbuild iRacingReplayOverlay.net.csproj -t:publish -p:"InstallUrl=http://iracingreplaydirector-test.s3-ap-southeast-2.amazonaws.com/" -p:ApplicationVersion=%APPVEYOR_BUILD_VERSION% -v:minimal -p:ProductName="iRacing Replay Director (test)" -p:OverrideAssemblyName=iRacingReplayOverlay.test
 rd /S /Q "Application Files"
-move "bin\x64\Debug\app.publish\setup.exe" setup.exe
-move "bin\x64\Debug\app.publish\iRacingReplayOverlay.test.application" iRacingReplayOverlay.test.application
-move "bin\x64\Debug\app.publish\Application Files" "Application Files"
 
-appveyor PushArtifact setup.exe -DeploymentName deploy-test
-appveyor PushArtifact iRacingReplayOverlay.test.application -DeploymentName deploy-test
+appveyor PushArtifact bin\x64\Debug\iRacingReplayOverlay.exe -FileName "versions\iRacingReplayOverlay.test.%APPVEYOR_BUILD_NUMBER%.exe" -DeploymentName deploy-test 
 
+cd bin\x64\Debug\app.publish
 for /D %%F in ("Application Files\*") do (
 	appveyor PushArtifact "%%F\iRacingReplayOverlay.test.exe.config.deploy" -FileName "%%F\iRacingReplayOverlay.test.exe.config.deploy" -DeploymentName deploy-test 
 	appveyor PushArtifact "%%F\iRacingReplayOverlay.test.exe.deploy"        -FileName "%%F\iRacingReplayOverlay.test.exe.deploy"        -DeploymentName deploy-test 
 	appveyor PushArtifact "%%F\iRacingReplayOverlay.test.exe.manifest"      -FileName "%%F\iRacingReplayOverlay.test.exe.manifest"      -DeploymentName deploy-test 
 )
 
+appveyor PushArtifact setup.exe -DeploymentName deploy-test
+appveyor PushArtifact iRacingReplayOverlay.test.application -DeploymentName deploy-test
 
-
+cd ..\..\..\..
