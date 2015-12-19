a14c326dbd9166bde1ae216082f5ba20cf5ed299
Dean Netherton <dean.netherton@gmail.com>
2015-12-19 13:12:05 +1100

    Package: more experiments

--- tools/package.cmd
+++ tools/package.cmd
@@ -1,4 +1,4 @@
-
+@echo off
 echo 'Building test package' %APPVEYOR_BUILD_NUMBER%
 
 msbuild iRacingReplayOverlay.net.csproj -t:publish -p:"InstallUrl=http://iracingreplaydirector-test.s3-ap-southeast-2.amazonaws.com/" -p:ApplicationVersion=%APPVEYOR_BUILD_VERSION% -v:minimal -p:ProductName="iRacing Replay Director (test)" -p:OverrideAssemblyName=iRacingReplayOverlay.test
@@ -6,13 +6,17 @@ rd /S /Q "Application Files"
 move "bin\x64\Debug\app.publish\setup.exe" setup.exe
 move "bin\x64\Debug\app.publish\iRacingReplayOverlay.test.application" iRacingReplayOverlay.test.application
 move "bin\x64\Debug\app.publish\Application Files" "Application Files"
-REM cd deploy\test
-REM 7z a .\..\..\deploy-test.zip .\**\*
-REM cd .\..\..
 
+REM appveyor PushArtifact setup.exe -DeploymentName deploy-test
+REM sappveyor PushArtifact iRacingReplayOverlay.test.application -DeploymentName deploy-test
 
-appveyor PushArtifact setup.exe -DeploymentName deploy-test
-appveyor PushArtifact iRacingReplayOverlay.test.application -DeploymentName deploy-test
-appveyor PushArtifact "Application Files" -DeploymentName deploy-test
+cd "Application Files"
+for /D %%F in (*) do (
+	appveyor PushArtifact "Application Files\%%F\iRacingReplayOverlay.test.exe.config.deploy" -DeploymentName deploy-test 
+	appveyor PushArtifact "Application Files\%%F\iRacingReplayOverlay.test.exe.deploy" -DeploymentName deploy-test 
+	appveyor PushArtifact "Application Files\%%F\iRacingReplayOverlay.test.exe.manifest" -DeploymentName deploy-test 
+)
+
+cd ..
 
 
