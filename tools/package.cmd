da504e977212725d003c1600ec2b728fc41a62d2
Dean Netherton <dean.netherton@gmail.com>
2015-12-19 12:06:51 +1100

    Packaging: script to pacakge and upload to appveyor

--- tools/package.cmd
+++ tools/package.cmd
@@ -1,8 +1,12 @@
 
 echo 'Building test package' %APPVEYOR_BUILD_NUMBER%
 
-msbuild iRacingReplayOverlay.net.csproj -t:publish -p:"InstallUrl=http://iracing-replay-director.s3-ap-southeast-2.amazonaws.com/test" -p:ApplicationVersion=%APPVEYOR_BUILD_VERSION% -v:minimal -p:ProductName="iRacing Replay Director (test)" -p:OverrideAssemblyName=iRacingReplayOverlay.test
+msbuild iRacingReplayOverlay.net.csproj -t:publish -p:"InstallUrl=http://iracingreplaydirector-test.s3-ap-southeast-2.amazonaws.com/" -p:ApplicationVersion=%APPVEYOR_BUILD_VERSION% -v:minimal -p:ProductName="iRacing Replay Director (test)" -p:OverrideAssemblyName=iRacingReplayOverlay.test
 rd /S /Q "deploy\test\Application Files"
 move "bin\x64\Debug\app.publish\*" deploy\test
 move "bin\x64\Debug\app.publish\Application Files" "deploy\test\Application Files"
+cd deploy\test
+7z a .\..\..\deploy-test.zip .\**\*
+cd .\..\..
+appveyor PushArtifact deploy-test.zip
 
