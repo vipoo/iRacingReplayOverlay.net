66e8d1e07ede6a090e69d107f0c987a31d7b28ec
Dean Netherton <dean.netherton@gmail.com>
2015-12-20 19:05:09 +1100

    Packaging: Added main release package

--- tools/package.cmd
+++ tools/package.cmd
@@ -2,7 +2,6 @@
 echo 'Building test package' %APPVEYOR_BUILD_VERSION%
 
 msbuild iRacingReplayOverlay.net.csproj -t:rebuild -t:publish -p:"InstallUrl=http://iracing-replay-director.s3-ap-southeast-2.amazonaws.com/test/" -p:ApplicationVersion=%APPVEYOR_BUILD_VERSION% -v:minimal -p:ProductName="iRacing Replay Director (test)" -p:OverrideAssemblyName=iRacingReplayOverlay.test
-rd /S /Q "Application Files"
 
 appveyor PushArtifact bin\x64\Debug\iRacingReplayOverlay.test.exe -FileName "versions\iRacingReplayOverlay.%APPVEYOR_BUILD_VERSION%.exe" -DeploymentName deploy-test 
 
@@ -21,7 +20,6 @@ cd ..\..\..\..
 echo 'Building beta package' %APPVEYOR_BUILD_VERSION%
 
 msbuild iRacingReplayOverlay.net.csproj -t:rebuild -t:publish -p:"InstallUrl=http://iracing-replay-director.s3-ap-southeast-2.amazonaws.com/beta/" -p:ApplicationVersion=%APPVEYOR_BUILD_VERSION% -v:minimal -p:ProductName="iRacing Replay Director (beta)" -p:OverrideAssemblyName=iRacingReplayOverlay.beta
-rd /S /Q "Application Files"
 
 cd bin\x64\Debug\app.publish
 for /D %%F in ("Application Files\*") do (
@@ -32,5 +30,21 @@ for /D %%F in ("Application Files\*") do (
 
 appveyor PushArtifact setup.exe                             -FileName "beta\setup.exe"                              -DeploymentName deploy-beta
 appveyor PushArtifact iRacingReplayOverlay.beta.application -FileName "beta\iRacingReplayOverlay.beta.application"  -DeploymentName deploy-beta
+
+cd ..\..\..\..
+
+echo 'Building main package' %APPVEYOR_BUILD_VERSION%
+
+msbuild iRacingReplayOverlay.net.csproj -t:rebuild -t:publish -p:"InstallUrl=http://iracing-replay-director.s3-ap-southeast-2.amazonaws.com/main/" -p:ApplicationVersion=%APPVEYOR_BUILD_VERSION% -v:minimal -p:ProductName="iRacing Replay Director" -p:OverrideAssemblyName=iRacingReplayOverlay
+
+cd bin\x64\Debug\app.publish
+for /D %%F in ("Application Files\*") do (
+	appveyor PushArtifact "%%F\iRacingReplayOverlay.exe.config.deploy" -FileName "main\%%F\iRacingReplayOverlay.exe.config.deploy" -DeploymentName deploy-main 
+	appveyor PushArtifact "%%F\iRacingReplayOverlay.exe.deploy"        -FileName "main\%%F\iRacingReplayOverlay.exe.deploy"        -DeploymentName deploy-main 
+	appveyor PushArtifact "%%F\iRacingReplayOverlay.exe.manifest"      -FileName "main\%%F\iRacingReplayOverlay.exe.manifest"      -DeploymentName deploy-main 
+)
+
+appveyor PushArtifact setup.exe                        -FileName "main\setup.exe"                         -DeploymentName deploy-main
+appveyor PushArtifact iRacingReplayOverlay.application -FileName "main\iRacingReplayOverlay.application"  -DeploymentName deploy-main
 
 cd ..\..\..\..
