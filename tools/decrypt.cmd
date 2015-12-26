b47c724615e4a70af7b62907e8272ed3b9a27c6c
Dean Netherton <dean.netherton@gmail.com>
2015-12-26 11:37:10 +1100

    AwsKeys: fixed decrypt command

--- tools/decrypt.cmd
+++ tools/decrypt.cmd
@@ -1,1 +1,1 @@
-packages/secure-file.1.0.31/tools/secure-file.exe -decrypt ./Aws/AwsKey.cs.enc -out ./Aws/AwsKey.cs -secret %SECRET%
+packages\secure-file.1.0.31\tools\secure-file.exe -decrypt .\Aws\AwsKey.cs.enc -out .\Aws\AwsKey.cs -secret %SECRET%
