5674b5afde2d59488b8d9bb02dc1494472ec224c
Dean Netherton <dean.netherton@gmail.com>
2015-12-29 12:04:54 +1100

    DeploymentPackage: Another test of changing test url from http to https

--- tools/package.cmd
+++ tools/package.cmd
@@ -1,7 +1,17 @@
 @echo off
 echo 'Building test package' %APPVEYOR_BUILD_VERSION%
 
-msbuild iRacingReplayOverlay.net.csproj -p:SolutionDir=%cd%\ -p:Configuration=Release -t:rebuild -t:publish -p:"InstallUrl=https://iracing-replay-director.s3-ap-southeast-2.amazonaws.com/stest/" -p:ApplicationVersion=%APPVEYOR_BUILD_VERSION% -v:minimal -p:ProductName="iRacing Replay Director (test)" -p:OverrideAssemblyName=iRacingReplayOverlay.test
+msbuild iRacingReplayOverlay.net.csproj -p:SolutionDir=%cd%\ 
+                                        -p:Configuration=Release 
+                                        -p:UpdateRequired=true ^
+                                        -p:MinimumRequiredVersion=%APPVEYOR_BUILD_VERSION%  ^
+                                        -t:rebuild 
+                                        -t:publish 
+                                        -p:"InstallUrl=https://iracing-replay-director.s3-ap-southeast-2.amazonaws.com/stest/" 
+                                        -p:ApplicationVersion=%APPVEYOR_BUILD_VERSION% 
+                                        -v:minimal 
+                                        -p:ProductName="iRacing Replay Director (test)" 
+                                        -p:OverrideAssemblyName=iRacingReplayOverlay.test
 
 appveyor PushArtifact bin\x64\Release\iRacingReplayOverlay.test.exe -FileName "versions\iRacingReplayOverlay.%APPVEYOR_BUILD_VERSION%.exe" -DeploymentName deploy-test 
 
@@ -15,6 +25,35 @@ for /D %%F in ("Application Files\*") do (
 
 appveyor PushArtifact setup.exe                             -FileName "stest\setup.exe"                              -DeploymentName deploy-test
 appveyor PushArtifact iRacingReplayOverlay.test.application -FileName "stest\iRacingReplayOverlay.test.application"  -DeploymentName deploy-test
+
+
+
+msbuild iRacingReplayOverlay.net.csproj -p:SolutionDir=%cd%\ ^
+                                        -p:Configuration=Release  ^
+                                        -p:UpdateRequired=true ^
+                                        -p:"UpdateUrl=https://iracing-replay-director.s3-ap-southeast-2.amazonaws.com/stest/"  ^
+                                        -p:MinimumRequiredVersion=1.0.1.78  ^
+                                        -t:rebuild  ^
+                                        -t:publish  ^
+                                        -p:"InstallUrl=http://iracing-replay-director.s3-ap-southeast-2.amazonaws.com/test/"  ^
+                                        -p:ApplicationVersion=1.0.1.78  ^
+                                        -v:minimal  ^
+                                        -p:ProductName="iRacing Replay Director (test)"  ^
+                                        -p:OverrideAssemblyName=iRacingReplayOverlay.test
+
+cd bin\x64\Release\app.publish
+for /D %%F in ("Application Files\*") do (
+	appveyor PushArtifact "%%F\iRacingReplayOverlay.test.exe.config.deploy" -FileName "test\%%F\iRacingReplayOverlay.test.exe.config.deploy" -DeploymentName deploy-test 
+	appveyor PushArtifact "%%F\iRacingReplayOverlay.test.exe.deploy"        -FileName "test\%%F\iRacingReplayOverlay.test.exe.deploy"        -DeploymentName deploy-test 
+	appveyor PushArtifact "%%F\iRacingReplayOverlay.test.pdb.deploy"        -FileName "test\%%F\iRacingReplayOverlay.test.pdb.deploy"        -DeploymentName deploy-test 
+	appveyor PushArtifact "%%F\iRacingReplayOverlay.test.exe.manifest"      -FileName "test\%%F\iRacingReplayOverlay.test.exe.manifest"      -DeploymentName deploy-test 
+)
+
+appveyor PushArtifact setup.exe                             -FileName "test\setup.exe"                              -DeploymentName deploy-test
+appveyor PushArtifact iRacingReplayOverlay.test.application -FileName "test\iRacingReplayOverlay.test.application"  -DeploymentName deploy-test
+
+
+
 
 cd ..\..\..\..
 
