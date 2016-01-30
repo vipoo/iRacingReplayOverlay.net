0310a8d5989483476698f1834f5fdf7780196f6f
Dean Netherton <dean.netherton@gmail.com>
2016-01-30 15:00:19 +1100

    Removed click-once deployment settings.  Removed ilmerging of all assemblies

--- tools/package.cmd
+++ tools/package.cmd
@@ -1,20 +1,19 @@
 @echo off
 echo 'Building test package' %APPVEYOR_BUILD_VERSION%
 
-msbuild iRacingReplayOverlay.net.csproj -p:SolutionDir=%cd%\                                                                    ^
-                                        -p:Configuration=Release                                                                ^
-                                        -t:rebuild                                                                              ^
-                                        -p:ApplicationVersion=%APPVEYOR_BUILD_VERSION%                                          ^
-                                        -v:minimal                                                                              ^
-                                        -p:ProductName="iRacing Replay Director"                                         ^
+msbuild iRacingReplayOverlay.net.csproj -p:SolutionDir=%cd%\                               ^
+                                        -p:Configuration=Release                           ^
+                                        -t:rebuild                                         ^
+                                        -p:ApplicationVersion=%APPVEYOR_BUILD_VERSION%     ^
+                                        -v:minimal                                         ^
+                                        -p:ProductName="iRacing Replay Director"           ^
                                         -p:OverrideAssemblyName=iRacingReplayOverlay
 
-copy bin\x64\Release\iRacingReplayOverlay.exe          iRacingReplayOverlay.exe
-copy bin\x64\Release\iRacingReplayOverlay.pdb          iRacingReplayOverlay.pdb
-copy bin\x64\Release\iRacingReplayOverlay.exe.config   iRacingReplayOverlay.exe.config
-copy bin\x64\Release\iRacingReplayOverlay.exe.manifest iRacingReplayOverlay.exe.manifest
+cd bin\x64\Release
 
-7z a release.zip iRacingReplayOverlay.exe  iRacingReplayOverlay.pdb iRacingReplayOverlay.exe.config iRacingReplayOverlay.exe.manifest
+7z a release.zip *.*
 
 appveyor PushArtifact release.zip -FileName "release.zip" -DeploymentName deploy-release
+
+cd ..\..\..
  
