c6afce15090b0fafff1a3cc3bad0b7d18bb7f991
Dean Netherton <dean.netherton@gmail.com>
2016-02-20 17:22:10 +1100

    Added common support assembly for plugins - and updated nuget packaging

--- tools/package.cmd
+++ tools/package.cmd
@@ -1,7 +1,8 @@
 SET SolutionDir=%~dp0..\
 SET ProjectDir=%~dp0..\iRacingDirector.Plugin.Tester\
 
-msbuild %ProjectDir%iRacingDirector.Plugin.Tester.csproj -p:SolutionDir=%SolutionDir% -t:rebuild -p:ApplicationVersion=%APPVEYOR_BUILD_VERSION% -v:minimal -p:Configuration=Release
+msbuild %SolutionDir%iRacingDirector.Plugin.Tester\iRacingDirector.Plugin.Tester.csproj   -p:SolutionDir=%SolutionDir% -t:rebuild -p:ApplicationVersion=%APPVEYOR_BUILD_VERSION% -v:minimal -p:Configuration=Release
+msbuild %SolutionDir%iRacingDirector.Plugin.Support\iRacingDirector.Plugin.Support.csproj -p:SolutionDir=%SolutionDir% -t:rebuild -p:ApplicationVersion=%APPVEYOR_BUILD_VERSION% -v:minimal -p:Configuration=Release
 
 %SolutionDir%tools\nuget.exe pack %SolutionDir%iRacingDirector.Plugin.Support.nuspec -Prop Configuration=release -Verbosity detail -Version %APPVEYOR_BUILD_VERSION%
 
