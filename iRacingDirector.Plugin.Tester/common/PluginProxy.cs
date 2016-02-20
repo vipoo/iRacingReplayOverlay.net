1bfadf9cefb0da2ff8343ce0c299093c44f8ea35
Dean Netherton <dean.netherton@gmail.com>
2016-02-20 16:23:37 +1100

    Updated TODO list

--- iRacingDirector.Plugin.Tester/common/PluginProxy.cs
+++ iRacingDirector.Plugin.Tester/common/PluginProxy.cs
@@ -5,18 +5,12 @@ using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Reflection;
 
-//TODO: Add plugin management form to main app
-//TODO: Discover overlay plugins, and show list in form
-//TODO: Allow user to 'select' a plugin, and use that plugin during transcoding
-//TODO: Show the plugin's settings & allow user to change.
-//TODO: Move all leaderboard graphiccs into plugin
-//TODO: Create plugin test app - to draw on a single bitmap
-//TODO: Creating publishing model - allow app to install different versions of plugin.
-//TODO: Ensure good error feedback if plugin missing stuff (eg: Graphics field)
-//TODO: Move support files from overlayplugin to plugin base
-//TODO: Add lots more style helpers
-//TODO: Additional meta data for plugin - name, description, author, etc. - use assembly attributes
-//TODO: Extract timing logic into seperate methods
+//TODO: Add timing, leaderboard and closing titles to overlay
+//TODO: Provide user configuration for settings
+//TODO: Add 'Font' type of configuration
+//TODO: Auto reload plugin on assembly update
+//TODO: Enable StandardOverlay to be loaded as single project solution
+//TODO: Move support code from overlay plugin into iRacingDirector.Plugin.Support
 
 namespace iRacingDirector
 {
@@ -63,7 +57,7 @@ namespace iRacingDirector
             graphics.CompositingQuality = CompositingQuality.HighQuality;
             graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
 
-            ((dynamic)plugin).Graphics = graphics;
+            plugin.Graphics = graphics;
         }
     }
 }
