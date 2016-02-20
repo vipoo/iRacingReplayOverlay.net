147e209892004d326e602d6b3d927d3906d1ab04
Dean Netherton <dean.netherton@gmail.com>
2016-02-21 10:06:59 +1100

    DescriptionAttribute for use in describing settings

---
+++ iRacingDirector.Plugin.Support/DescriptionAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace iRacingDirector.Plugin
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class DescriptionAttribute : Attribute
+    {
+        public string Description { get; private set; }
+
+        public DescriptionAttribute(string description)
+        {
+            this.Description = description;
+        }
+
+    }
+}
