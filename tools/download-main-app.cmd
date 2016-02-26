fb3827cecdfa21d1df6de42da44e61fd89c68b2e
Dean Netherton <dean.netherton@gmail.com>
2016-02-26 13:20:26 +1100

    Packaging: Include iRacingReplayOverlay.exe in nuget package

--- tools/download-main-app.cmd
+++ tools/download-main-app.cmd
@@ -5,6 +5,6 @@ rm -rf iRacingReplayOverlay.net.%IRACING_REPLAY_OVERLAY%
 mkdir iRacingReplayOverlay.net.%IRACING_REPLAY_OVERLAY%
 cd iRacingReplayOverlay.net.%IRACING_REPLAY_OVERLAY%
 curl -L https://github.com/vipoo/iRacingReplayOverlay.net/releases/download/%IRACING_REPLAY_OVERLAY%/release.zip --output release.zip
-7z e release.zip
+7z x release.zip
 rm release.zip
 cd .\..\..
