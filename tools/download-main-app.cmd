17f4658b20d98aa552adc9e2ef60b94c836664aa
Dean Netherton <dean.netherton@gmail.com>
2016-02-26 12:52:00 +1100

    Prebuild step to download iRacingReplayOverlay.net release

---
+++ tools/download-main-app.cmd
@@ -0,0 +1,10 @@
+set IRACING_REPLAY_OVERLAY=1.0.3.17
+
+cd packages
+rm -rf iRacingReplayOverlay.net.%IRACING_REPLAY_OVERLAY%
+mkdir iRacingReplayOverlay.net.%IRACING_REPLAY_OVERLAY%
+cd iRacingReplayOverlay.net.%IRACING_REPLAY_OVERLAY%
+curl -L https://github.com/vipoo/iRacingReplayOverlay.net/releases/download/%IRACING_REPLAY_OVERLAY%/release.zip --output release.zip
+7z e release.zip
+rm release.zip
+cd .\..\..
