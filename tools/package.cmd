3dde9363ac50f84bd972818cb11b794ba485bcf1
Dean Netherton <dean.netherton@gmail.com>
2016-02-27 14:13:36 +1100

    Tester: Moved to main application's release

--- tools/package.cmd
+++ tools/package.cmd
@@ -1,6 +1,5 @@
 SET SolutionDir=%~dp0..\
 
-msbuild %SolutionDir%iRacingDirector.Plugin.Tester\iRacingDirector.Plugin.Tester.csproj   -p:SolutionDir=%SolutionDir% -t:rebuild -p:ApplicationVersion=%APPVEYOR_BUILD_VERSION% -v:minimal -p:Configuration=Release
 msbuild %SolutionDir%iRacingDirector.Plugin.Support\iRacingDirector.Plugin.Support.csproj -p:SolutionDir=%SolutionDir% -t:rebuild -p:ApplicationVersion=%APPVEYOR_BUILD_VERSION% -v:minimal -p:Configuration=Release
 
 %SolutionDir%tools\nuget.exe pack %SolutionDir%iRacingDirector.Plugin.Support.nuspec -Prop Configuration=release -Verbosity detail -Version %APPVEYOR_BUILD_VERSION%
