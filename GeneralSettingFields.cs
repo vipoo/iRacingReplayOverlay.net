3ba75e811ca8a3feb1d3f8e062f4d3e42eede5a1
Merlin Cooper <MerlinC@gmx.de>
2019-02-02 22:36:14 +0100

    Allow to set timespan to be lower 1 sec for a gap between vehicles in a battle.
    Closes vipoo/iRacingReplayOverlay#105

--- GeneralSettingFields.cs
+++ GeneralSettingFields.cs
@@ -117,7 +117,13 @@ namespace iRacingReplayOverlay
             {
                 var newSeconds = 0.0;
                 if (double.TryParse(tb.Text, out newSeconds))
-                    settings[setting] = newSeconds.Seconds();
+                {
+                    if (string.Compare(setting, "BattleGap") == 0)
+                        settings[setting] = TimeSpan.FromMilliseconds(newSeconds*1000);
+                    else
+                        settings[setting] = newSeconds.Seconds(); 
+                }
+                  
             });
         }
 
