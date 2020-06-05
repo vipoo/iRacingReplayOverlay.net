b3ae4b5b13d8709cf5ed13f35923eb2d212982ed
Merlin Cooper <MerlinC@gmx.de>
2020-06-05 08:47:16 +0200

    add source code for NoOverlay
    Has been a submodule in the past but is no part of the solution iRacingReplayDirector

---
+++ plugins/NoOverlay/MyPlugin.cs
@@ -0,0 +1,46 @@
+﻿using iRacingDirector.Plugin;
+using iRacingSDK.Support;
+using System.Drawing;
+
+namespace NoOverlay
+{
+    public partial class MyPlugin
+    {
+        public Graphics Graphics;
+        public EventData EventData;
+
+        const int FlashCardWidth = 900;
+        const int FlashCardLeft = (1920 / 2) - FlashCardWidth / 2;
+        const int DriversPerPage = 10;
+                
+        public void IntroFlashCard(long duration, long timestamp)
+        {
+            //Do Nothing - below you can find a code example of JockeyOverlay
+
+            /*var page = FlashCardPagingCalculator.GetPageNumber(EventData, DriversPerPage, duration, timestamp);
+
+           DrawIntroFlashCard(page);*/
+        }
+
+        public void RaceOverlay(long timestamp)
+        {
+            //Do Nothing - below you can find a code example of JockeyOverlay
+
+            /*var timeInSeconds = timestamp.FromNanoToSeconds();
+
+            DrawLeaderboard(timeInSeconds.Seconds());
+            DrawCurrentDriverRow();
+            DrawRaceMessages(timeInSeconds);
+            DrawFastestLap();*/
+        }
+
+        public void OutroFlashCard(long duration, long timestamp)
+        {
+            //Do Nothing - below you can find a code example of JockeyOverlay
+
+            /*var page = FlashCardPagingCalculator.GetPageNumber(EventData, DriversPerPage, duration, timestamp);
+
+            DrawOutroFlashCard(page);*/
+        }
+    }
+}
---
+++ plugins/NoOverlay/Styles.cs
@@ -0,0 +1,32 @@
+﻿using iRacingDirector.Plugin;
+using System.Drawing;
+
+namespace NoOverlay
+{
+    public static class Styles
+    {
+        public const int AlphaLevel = 0;
+        public static readonly Color White = Color.FromArgb(AlphaLevel, Color.White);
+        public static readonly Color WhiteSmoke = Color.FromArgb(AlphaLevel, Color.WhiteSmoke);
+        public static readonly Color Black = Color.FromArgb(AlphaLevel, Color.Black);
+
+        public static readonly Color RedBannerDark = Color.DarkRed;
+        public static readonly Color RedBannerLight = Color.DarkRed.BrightenBy(1.4);
+
+        public static readonly Pen BlackPen = new Pen(Black);
+        public static readonly Pen ThickBlackPen = new Pen(Black, 2);
+        public static readonly Pen WhitePen = new Pen(White);
+
+        public static readonly Brush BlackBrush = new SolidBrush(Color.Black);
+        public static readonly Brush RedBrush = new SolidBrush(Color.Red);
+        public static readonly Brush WhiteBrush = new SolidBrush(Color.White);
+        public static readonly Brush YellowBrush = new SolidBrush(Color.Yellow);
+
+        public static readonly Brush TransparentLightBlack = new SolidBrush(Color.FromArgb(200, 30, 30, 30));
+
+        public static readonly Brush TransparentLightGray = new SolidBrush(Color.FromArgb(200, 60, 60, 60));
+        public static readonly Brush TransparentLighterGray = new SolidBrush(Color.FromArgb(40, Color.LightGray));
+
+        public static readonly Pen DividerLinePen = new Pen(Color.FromArgb(255, 40, 40, 40), 2);
+    }
+}
