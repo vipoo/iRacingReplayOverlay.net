76eb90b5b2591d546b9c8b5a0b318c0df38b5bb4
Kareem Sultan <kareemsultan@gmail.com>
2020-06-08 15:49:44 -0400

    removes all unecessary files from plugins projects

--- plugins/iRacingDirector.Plugin.Support/tools/package.cmd
+++
@@ -1,6 +0,0 @@
-SET SolutionDir=%~dp0..\
-
-msbuild %SolutionDir%iRacingDirector.Plugin.Support\iRacingDirector.Plugin.Support.csproj -p:SolutionDir=%SolutionDir% -t:rebuild -p:ApplicationVersion=%APPVEYOR_BUILD_VERSION% -v:minimal -p:Configuration=Release
-
-%SolutionDir%tools\nuget.exe pack %SolutionDir%iRacingDirector.Plugin.Support.nuspec -Prop Configuration=release -Verbosity detail -Version %APPVEYOR_BUILD_VERSION%
-
