b0dd9b3976643ad277617b25f1b9db590f93ae5c
Merlin Cooper <MerlinC@gmx.de>
2022-05-10 04:37:10 +0200

    Fixes #66: Failure causes by using wrong modifier key

--- Support/KeyboardEmulator.cs
+++ Support/KeyboardEmulator.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 using WK.Libraries.HotkeyListenerNS;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using iRacingSDK.Support;
 
 namespace iRacingReplayDirector.Support
 {
@@ -12,12 +9,19 @@ namespace iRacingReplayDirector.Support
     {
         [System.Runtime.InteropServices.DllImport("user32.dll")]
         public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+        //private KeyboardSimulator sim = new KeyboardSimulator();
 
         static public void SendKeyStrokes(Hotkey hotkey)
         {
             Keys modifierKey = hotkey.Modifiers;
-            Byte modifier = (Byte)modifierKey; 
-            Byte key = (Byte)(Keys)hotkey.KeyCode; 
+            Byte modifier = (Byte)modifierKey;
+            Byte key = (Byte)hotkey.KeyCode;
+
+            TraceDebug.WriteLine("keybd_event sent: modifier = {0} | key = {1}".F(modifier, key));
+
+            //REMARK: After updating to .NET 5.0 values could be easily converted to hex-string using "Convert.toHexString"
+
+            //TraceDebug.WriteLine("Race analysis phase completed. {0} data samples processed with replay speed {1}".F(numberOfDataProcessed, iReplaySpeedForAnalysis));
 
             keybd_event(modifier, 0, 0, UIntPtr.Zero);
             System.Threading.Thread.Sleep(700);
